cd89d313779f6c49c29959c9caec2ad2a509e984
benamiller <mrbenamiller@gmail.com>
2024-12-13 15:56:49 +0000

    Checks for nullish across all features

---
+++ README.md
@@ -0,0 +1,9 @@
+# Titanic - Beginner Kaggle Competition
+
+## Data
+
+https://www.kaggle.com/competitions/titanic/data
+
+## Purpose
+
+This project will be a proving ground for a CUDA and C library for fully connected neural networks
--- python/main.py
+++ python/main.py
@@ -6,5 +6,52 @@ print(np.zeros((1,2)))
 df = pd.read_csv('./data/train.csv')
 print(df.head(5))
 
-# Check for nullish Sex values
+## Nullish checks
+
+# PassengerIds
+print('PassengerIds')
+print(df[df['PassengerId'].isna()])
+
+# Survived
+print('Survived')
+print(df[df['Survived'].isna()])
+
+# Pclass (1st, 2nd, or 3rd)
+print('Pclass')
+print(df[df['Pclass'].isna()])
+
+# Name
+print('Name')
+print(df[df['Name'].isna()])
+
+# Sex
+print('Sex')
 print(df[df['Sex'].isna()])
+
+# Age
+print('Age')
+print(df[df['Age'].isna()])
+
+# SibSp (Number of siblings/spouses aboard
+print('SibSp')
+print(df[df['SibSp'].isna()])
+
+# Parch
+print('Parch')
+print(df[df['Parch'].isna()])
+
+# Ticket
+print('Ticket')
+print(df[df['Ticket'].isna()])
+
+# Fare
+print('Fare')
+print(df[df['Fare'].isna()])
+
+# Cabin
+print('Cabin')
+print(df[df['Cabin'].isna()])
+
+# Embarked (Port of Embarkation)
+print('Embarked')
+print(df[df['Embarked'].isna()])
